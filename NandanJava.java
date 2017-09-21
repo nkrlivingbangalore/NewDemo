a7e1574026a0f5a0a5d05ec0358710f14924d688
nkrlivingbangalore <nkrlivingbangalore@gmail.com>
2017-09-21 14:42:16 +0530

    hi i have changed file

--- NandanJava.java
+++ NandanJava.java
@@ -3,9 +3,6 @@ class NandanJava
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello World!");
-		System.out.println("Hello Nandan");
-
-		System.out.println("hi  nandan");
-		System.out.println("welcome nandan");
+	
 	}
 }
