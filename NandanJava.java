296cd4d103d3e547dda60fe46a9196cd0ca10ab6
nkrlivingbangalore <32154274+nkrlivingbangalore@users.noreply.github.com>
2017-09-21 14:53:14 +0530

    Update NandanJava.java
    hi guyes plz consider my excellent code is here.

--- NandanJava.java
+++ NandanJava.java
@@ -3,6 +3,7 @@ class NandanJava
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello World!");
+		System.out.println("Hello Nandan Kumar you are great person keep doing all is well!!!!!!!!!");
 	
 	}
 }
